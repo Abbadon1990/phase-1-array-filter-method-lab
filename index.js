a548c581c1a53d686e39f0fcd3e7faf57e0790de
Logan Wood <lgnwood@gmail.com>
2023-10-20 19:08:22 -0600

    complete tests

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(names, string) {
+    return names.filter((el) => el.toLowerCase().includes(string.toLowerCase()))
+}
+
+function fuzzyMatch(names, string) {
+    return names.filter((el) => el.toLowerCase().startsWith(string.toLowerCase()))
+}
+
+function matchName (names, string) {
+    return names.filter(el => el.name === string)
+}
